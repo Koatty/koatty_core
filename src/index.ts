c352d55a546aadfa980ccf16bb9f2ef633c29518
linyyyang <linyyyang@tencent.com>
2021-11-18 18:37:17 +0800

    ✨ feat: add KoattyMetadata

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@
  * @Usage:
  * @Author: richen
  * @Date: 2021-07-09 18:49:56
- * @LastEditTime: 2021-07-09 18:50:17
+ * @LastEditTime: 2021-11-18 17:51:30
  */
 export * from "./Application";
 export * from "./Context";
+export * from "./Metadata";
