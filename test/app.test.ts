158442764c4564c9a61e3483bc8bea30785762eb
richenlin <richenlin@gmail.com>
2024-11-20 16:25:48 +0800

    test: test case

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,7 +3,7 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2024-11-08 09:54:42
- * @LastEditTime: 2024-11-19 16:43:33
+ * @LastEditTime: 2024-11-20 16:08:48
  * @License: BSD (3-Clause)
  * @Copyright (c): <richenlin(at)gmail.com>
  */
@@ -43,7 +43,6 @@ describe("App", () => {
   })
 
   test("response", async () => {
-    const s = app.listen()
     const res = await request(app.callback()).get('/'); // 发起 GET 请求
     // 测试响应
     expect(res.text).toBe('Hello, World!');
